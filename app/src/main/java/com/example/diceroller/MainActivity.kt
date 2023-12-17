e81724ea48a0e3210ad61db8d2ddaa2713279bb3
Volodymyr Yevsiuk <vov2706@gmail.com>
2023-12-17 18:01:35 +0200

    Display the correct dice image based on the dice roll

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -28,7 +28,17 @@ class MainActivity : AppCompatActivity() {
 
         // Update the screen with the dice roll
         val diceImage: ImageView = findViewById(R.id.imageView)
-        diceImage.setImageResource(R.drawable.dice_2)
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
+        diceImage.contentDescription = diceRoll.toString()
     }
 }
 
